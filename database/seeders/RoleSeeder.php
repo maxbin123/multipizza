1b25e0cc0f9b5d724ab95d97da6a095156e85278
Max <max@sent.as>
2021-09-07 13:07:19 +0300

    Role model & phone

---
+++ database/seeders/RoleSeeder.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Role;
+use Illuminate\Database\Seeder;
+
+class RoleSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        Role::insert([
+            [
+                'name' => 'Administrator',
+                'slug' => 'admin'
+            ],
+            [
+                'name' => 'Customer',
+                'slug' => 'customer'
+            ],
+            [
+                'name' => 'Manager',
+                'slug' => 'manager'
+            ],
+            [
+                'name' => 'Delivery',
+                'slug' => 'deliver'
+            ],
+            [
+                'name' => 'Cook',
+                'slug' => 'cook'
+            ],
+        ]);
+    }
+}
---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Role;
+use App\Models\User;
+use Illuminate\Database\Seeder;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        User::create([
+            'name' => 'Admin',
+            'email' => 'admin@admin.com',
+            'phone' => '+79898055058',
+            'password' => 'admin',
+            'role_id' => Role::where('slug', 'admin')->first()->id,
+        ]);
+
+    }
+}
