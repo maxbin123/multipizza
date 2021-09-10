a77a0ef36d2b90c14e9380e81dd57d861e2d3e52
Max <max@sent.as>
2021-09-10 19:01:12 +0300

    Restaurant model distance limit coordinates based

--- app/Models/Restaurant.php
+++ app/Models/Restaurant.php
@@ -4,13 +4,30 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Builder;
+use Malhal\Geographical\Geographical;
 
 class Restaurant extends Model
 {
-    use HasFactory;
+    use HasFactory, Geographical;
+
+    const DISTANCE_LIMIT = 4;
+    protected static $kilometers = true;
 
     public function branch()
     {
         return $this->belongsTo(Branch::class);
+    }
+
+    protected static function booted()
+    {
+        // Client passes his coordinates in header, distance limit automatically applied
+        static::addGlobalScope('geofenced', function (Builder $query) {
+            $longitude = request()->header('X-Location-Longitude');
+            $latitude = request()->header('X-Location-Latitude');
+            if ($longitude && $latitude && !request()->query('full')) {
+                $query->geofence(floatval($longitude), floatval($latitude), 0, self::DISTANCE_LIMIT);
+            }
+        });
     }
 }
