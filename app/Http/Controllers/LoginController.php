dc91525707036aac6940a0fff1dcdafdc2dd17af
Max <max@sent.as>
2021-09-10 16:31:43 +0300

    API for logging in with OTP code

---
+++ app/Http/Controllers/LoginController.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+
+class LoginController extends Controller
+{
+    public function getOtp(Request $request)
+    {
+        $phone = $request->phone;
+        $user = User::where('phone', $phone)->first();
+        $token = app('otp')->create($user->id, $length = 4);
+        $user->notify($token->toNotification());
+        return $token->plainText();
+    }
+
+    public function loginWithOtp(Request $request)
+    {
+        $phone = $request->phone;
+        $otp = $request->otp;
+
+        $user = User::where('phone', $phone)->first();
+        if (app('otp')->check($user->id, $otp)) {
+            $token = $user->createToken('login');
+            return ['token' => $token->plainTextToken];
+        } else {
+            abort(403, 'Invalid OTP code');
+        }
+    }
+}
